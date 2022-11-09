2e91f7a0aec5af539d5f10f06f4d38558c387597
Thomas Kosta <biomyriad@gmail.com>
2022-11-09 02:34:52 -0800

    Flask app base

---
+++ flask_app/controllers/main_controller.py
@@ -0,0 +1,26 @@
+from flask_app import app
+from flask import render_template, request, redirect, url_for, session, flash
+
+#from flask_app.models.user import User
+
+# # # # # # # # # # #
+#   Routes 
+# # # # # # # # # # #
+
+@app.route('/')
+def route_landing():
+    return "hello" 
+    #return redirect("/login")  
+
+
+# # # # # # # # # # #
+#   Error 
+# # # # # # # # # # # 
+
+@app.errorhandler(404)
+def handle_404(ex):
+    return render_template("pages/error_pages/404_error.html")  
+
+@app.errorhandler(500)
+def handle_500(ex):
+    return render_template("pages/error_pages/500_error.html")  
