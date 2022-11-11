aa74a20fe795b79888edacce6393d4ae6c826550
Thomas Kosta <biomyriad@gmail.com>
2022-11-11 12:38:10 -0800

    Post page

--- flask_app/controllers/main_controller.py
+++ flask_app/controllers/main_controller.py
@@ -9,18 +9,23 @@ from flask import render_template, request, redirect, url_for, session, flash
 
 @app.route('/')
 def route_landing():
-    return "hello" 
+    return render_template("partials/login.html")
     #return redirect("/login")  
+
+@app.route('/')
+def route_test():
+    #return "hello" 
+    return render_template("pages/post_feed.html")  
 
 
 # # # # # # # # # # #
 #   Error 
 # # # # # # # # # # # 
 
-@app.errorhandler(404)
-def handle_404(ex):
-    return render_template("pages/error_pages/404_error.html")  
+# @app.errorhandler(404)
+# def handle_404(ex):
+#     return render_template("pages/error_pages/404_error.html")  
 
-@app.errorhandler(500)
-def handle_500(ex):
-    return render_template("pages/error_pages/500_error.html")  
+# @app.errorhandler(500)
+# def handle_500(ex):
+#     return render_template("pages/error_pages/500_error.html")  
